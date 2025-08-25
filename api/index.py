7c80351d0e786b14bf65e6fbe22886ba61115d1b
boouee <104202645+boouee@users.noreply.github.com>
2025-08-25 14:57:26 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Request
 #from tgbot.main import tgbot
 from api.imap_handler import imap_handler
+from api.new_comment_handler import new_comment_handler
 from urllib.parse import unquote, urlparse
+from pydantic import BaseModel
 
+class Body(BaseModel):
+    data: Data
+
+class Data(BaseModel):
+    ID: int
+    
 app = FastAPI()
-
 
 @app.get('/api/update')
 async def update(request: Request):
@@ -14,3 +21,12 @@ async def update(request: Request):
     except Exception as e:
         print(e)
 
+
+@app.post('/api/new-comment')
+async def update(body: Body):
+    print(body)
+    try:
+        ...
+        await new_comment_handler(body["data"]["ID"])
+    except Exception as e:
+        print(e)
