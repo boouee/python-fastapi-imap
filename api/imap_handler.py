25d8c8412a17970c7a463ca4582390c89bb4ad03
boouee <104202645+boouee@users.noreply.github.com>
2025-08-26 19:12:11 +0500

    Update imap_handler.py

--- api/imap_handler.py
+++ api/imap_handler.py
@@ -8,7 +8,7 @@ server = "mail.netangels.ru"
 #email - UF_CRM_1723114805999
 #name - UF_CRM_1723114789182
 #phone - UF_CRM_1723114796732
-api = "https://b24-d1uwq7.bitrix24.ru/rest/1/lh1jmrsp8p01x3j3/"
+api = "https://b24-d1uwq7.bitrix24.ru/rest/1/2ylw0cg8hm7u5ef4/"
 block_id_values = {'rec1158004176': 'UC_JJAT3O', 'rec1151357361': 'STORE' , '#rec1206105936': 'WEB' , '#rec860376858': 'UC_LC50KQ' }
 #block_id_values = {'rec1158004176': '572', 'rec1151357361': '31' , '#rec1206105936': '15' , '#rec860376858': '574' }
 
