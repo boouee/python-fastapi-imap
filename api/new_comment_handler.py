433782a8004abb4850717333defbc8b792fd38e2
boouee <104202645+boouee@users.noreply.github.com>
2025-08-27 16:00:56 +0500

    Update new_comment_handler.py

--- api/new_comment_handler.py
+++ api/new_comment_handler.py
@@ -4,16 +4,22 @@ api = "https://b24-d1uwq7.bitrix24.ru/rest/1/2ylw0cg8hm7u5ef4/"
 
 async def new_comment_handler(comment):
   async with httpx.AsyncClient() as client:
-    company = await get_company(client, comment)
+    company = await get_company_id(client, comment)
+    title = await get_company_name(client, company)
     await send_notification(client, company)
 
-async def get_company(client, comment):
+async def get_company_id(client, comment):
   response = await client.get(f"{api}crm.timeline.comment.get?id={comment}")
   response = response.json()
   return response["result"]["ENTITY_ID"]
 
 async def send_notification(client, company):
-  data = {"USER_ID": 1, "MESSAGE": f"Новый комментарий на {company}"}
+  data = {"USER_ID": 1, "MESSAGE": f"<h1>Новый комментарий</h1> на https://b24-d1uwq7.bitrix24.ru/crm/company/details/{company}"}
   response = await client.post(f"{api}im.notify.system.add", json=data)
   response = response.json()
   return response["result"]["ENTITY_ID"]
+
+async def get_company_name(client, company):
+  response = await client.get(f"{api}crm.company.get?id={company}")
+  response = response.json()
+  return response["result"]["TITLE"]
