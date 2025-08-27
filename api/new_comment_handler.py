4b89665b319057ccb5361c3155ca09cb9bdc6010
boouee <104202645+boouee@users.noreply.github.com>
2025-08-27 16:12:45 +0500

    Update new_comment_handler.py

--- api/new_comment_handler.py
+++ api/new_comment_handler.py
@@ -6,14 +6,14 @@ async def new_comment_handler(comment):
   async with httpx.AsyncClient() as client:
     company = await get_company_id(client, comment)
     title = await get_company_name(client, company)
-    await send_notification(client, company,title)
+    await send_notification(client, company, title)
 
 async def get_company_id(client, comment):
   response = await client.get(f"{api}crm.timeline.comment.get?id={comment}")
   response = response.json()
   return response["result"]["ENTITY_ID"]
 
-async def send_notification(client, company):
+async def send_notification(client, company, title):
   data = {"USER_ID": 1, "MESSAGE": f"Новый комментарий на [URL=https://b24-d1uwq7.bitrix24.ru/crm/company/details/{company}/]{title}[/URL]"}
   response = await client.post(f"{api}im.notify.system.add", json=data)
   response = response.json()
